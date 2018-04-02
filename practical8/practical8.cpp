cf3a23908ee65489dc63230b3a711f3af098cefe
DavidMc10 <Davidmcelhinney10@hotmail.com>
2018-04-02 20:31:30 +0100

    Add project files.

---
+++ practical8/practical8.cpp
@@ -0,0 +1,45 @@
+//================================================================================
+// Name        : practical8.cpp
+// Author      : a.mccaughey@ulster.ac.uk
+// Version     : 1.0
+// Description : practical 8 - program to utilise BinarySearch tree 
+//================================================================================
+
+#include <iostream>
+#include <string>
+
+#include "BinaryTree.h"
+
+using namespace std;
+
+
+
+// create and manipulate a binary tree
+void testTree() {
+
+	// Construct tree from Q1
+
+	// Display the tree in-order, pre-order and post-order
+
+	// Remove elements and display tree again
+	
+	// test find method - verify that element was previously deleted
+	
+	// Clear tree and add numbers in order
+	
+	// redisplay tree and its new height
+	
+}
+
+// Main method.
+int main()
+{
+	testTree();
+
+	// ---------------------------------------------------
+	std::cout << std::endl << "Press enter to quit";
+	std::cin.sync(); // flush input buffer
+	std::cin.ignore(); // ignore what user enters
+
+	return 0;
+}
